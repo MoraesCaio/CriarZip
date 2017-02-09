f922de74e230a8ea86538322e34b5baf8e3fc9ad
MoraesCaio <MoraesCaio>
2017-02-09 17:37:35 -0200

    Remocao do requisitos.zip e Adicao de incrementadores
    Automizacao do incremento no valor de Revision em version.json e
    versionPython.json.

--- CriarZip/Program.cs
+++ CriarZip/Program.cs
@@ -5,6 +5,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Web.Script.Serialization;
 
 namespace CriarZip
 {
@@ -57,29 +58,62 @@ namespace CriarZip
             //PASTAS
             string dir = Directory.GetCurrentDirectory();
             string VLIBRAS = Path.Combine(dir, @"VLIBRAS\");
-            string requisitos = Path.Combine(dir, @"requisitos\");
+            //string requisitos = Path.Combine(dir, @"requisitos\");
             string enviar = Path.Combine(dir, @"enviar\");
             string python = Path.Combine(dir, @"python\");
             string release = Path.Combine(dir, @"release\");
 
             Zip zip_VLIBRAS    = new Zip(VLIBRAS, Path.Combine(enviar, "VLIBRAS.zip"), true);
-            Zip zip_requisitos = new Zip(requisitos, Path.Combine(enviar, "requisitos.zip"), true);
-            Zip zip_python     = new Zip(python, Path.Combine(release, "python.zip"), true);
+            //Zip zip_requisitos = new Zip(requisitos, Path.Combine(enviar, "requisitos.zip"), true);
+            Zip zip_python     = new Zip(python, Path.Combine(enviar, "python.zip"), true);
 
             Thread t_VLIBRAS    = new Thread(new ThreadStart(zip_VLIBRAS.zipFromDirectory));
-            Thread t_requisitos = new Thread(new ThreadStart(zip_requisitos.zipFromDirectory));
+            //Thread t_requisitos = new Thread(new ThreadStart(zip_requisitos.zipFromDirectory));
             Thread t_python     = new Thread(new ThreadStart(zip_python.zipFromDirectory));
 
             t_VLIBRAS.Start();
-            t_requisitos.Start();
+            //t_requisitos.Start();
             t_python.Start();
 
             Thread.Sleep(1);
 
             t_VLIBRAS.Join();
-            t_requisitos.Join();
+            //t_requisitos.Join();
             t_python.Join();
 
+            //version.json e versionPython.json
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            StreamReader sr = new StreamReader(Path.Combine(enviar,@"version.json"));
+            string version = sr.ReadToEnd();
+            dynamic arrayVersion = serializer.DeserializeObject(version);
+            arrayVersion["Revision"] += 1;
+            string novoArrayVersion = serializer.Serialize(arrayVersion);
+            sr.Close();
+            File.WriteAllText(Path.Combine(enviar,@"version.json"), novoArrayVersion);
+
+            sr = new StreamReader(Path.Combine(enviar,@"versionPython.json"));
+            string pythonJson = sr.ReadToEnd();
+            dynamic arrayPython = serializer.DeserializeObject(pythonJson);
+            arrayPython["Revision"] += 1;
+            string novoArrayPython = serializer.Serialize(arrayPython);
+            sr.Close();
+            File.WriteAllText(Path.Combine(enviar,@"versionPython.json"), novoArrayPython);
+
+            int vMaj = Convert.ToInt32(arrayVersion["Major"]);
+            int vMin = Convert.ToInt32(arrayVersion["Minor"]);
+            int vBui = Convert.ToInt32(arrayVersion["Build"]);
+            int vRev = Convert.ToInt32(arrayVersion["Revision"]);
+
+            int pMaj = Convert.ToInt32(arrayPython["Major"]);
+            int pMin = Convert.ToInt32(arrayPython["Minor"]);
+            int pBui = Convert.ToInt32(arrayPython["Build"]);
+            int pRev = Convert.ToInt32(arrayPython["Revision"]);
+
+            Console.WriteLine("\nversion.json\nMajor: {0}\nMinor: {1}\nBuild: {2}\nRevision: {3}", vMaj, vMin, vBui, vRev);
+            Console.WriteLine("\nversionPython.json\nMajor: {0}\nMinor: {1}\nBuild: {2}\nRevision: {3}", pMaj, pMin, pBui, pRev);
+
+            //sinais
             if(File.Exists(Path.Combine(release, "sinais.txt"))){
                 Console.WriteLine("\nO arquivo de lista de sinais está presente na pasta release.");
             }else{
